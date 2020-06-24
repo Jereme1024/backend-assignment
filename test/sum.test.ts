a2e6828bba0ab04e79693287fce4e1306c1d250c
Jeremy <thestyle1202@gmail.com>
2020-06-24 22:49:02 +0800

    correct format

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,13 +1,14 @@
-import sum from '../src/sum';
+import sum from '../src/sum'
 
 test('adds 1 + 2 to equal 3', () => {
-  // Arrange
-  let x: number = 1, y: number = 2;
-  let expected: number = 3;
+    // Arrange
+    let x: number = 1,
+        y: number = 2
+    let expected: number = 3
 
-  // Act
-  let actual: number = sum(x, y);
+    // Act
+    let actual: number = sum(x, y)
 
-  // Assert
-  expect(actual).toBe(expected);
-});
+    // Assert
+    expect(actual).toBe(expected)
+})
