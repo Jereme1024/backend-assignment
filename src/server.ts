e7ae852724f286394ba57f30b8aab30006354f56
Jeremy <thestyle1202@gmail.com>
2020-06-25 21:49:53 +0800

    add a basic server with API set

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,43 @@
 import express = require('express')
+import { HeroModel } from './hero-model'
 
 const app: express.Application = express()
 
-app.get('/', function (req, res) {
+const v1 = express.Router()
+
+let heroModel: HeroModel
+
+v1.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+v1.get('/heroes', (req, res) => {
+    let isAuth = false
+    let heroes = heroModel.getHeroes()
+    if (!isAuth) {
+        for (let hero of heroes) {
+            delete hero.profile
+        }
+    }
+
+    res.send(heroes)
+})
+
+v1.get('/heroes/:heroId', (req, res) => {
+    let isAuth = false
+    if (req.headers.name && req.headers.password) {
+        isAuth = true
+    }
+    const heroId = req.params.heroId
+    const hero = heroModel.getHero(heroId)
+    if (!isAuth) {
+        delete hero?.profile
+    }
+    res.send(hero ? hero : 'Not Found')
+})
+
+app.use('/v1', v1)
+app.use('/', v1)
 
 app.listen(3000, function () {
     console.log('App is listening on port 3000!')
